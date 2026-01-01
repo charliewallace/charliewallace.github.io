a052536b668d9ef515803fa333fa6e225e3d66e1
Charles L. Wallace <charliewallace@gmail.com>
2025-12-31 22:37:01 -0800

    chore: bump app version to v0.3.7

--- sketch.js
+++ sketch.js
@@ -65,7 +65,7 @@ Future Enhancement Ideas ------------
 
 //======== GLOBALS ===================================
 // Name convention: global vars are capitalized
-const APP_VERSION = "v0.3.6 ©2026 Charlie Wallace";
+const APP_VERSION = "v0.3.7 ©2026 Charlie Wallace";
 
 console.log("📦 Day Spiral Clock loaded");
 var WebsiteLink;
